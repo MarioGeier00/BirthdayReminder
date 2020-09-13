8710a79219fdd9d7ad86e47c6b9becd934ab5d85
MarioGeier00 <55717820+MarioGeier00@users.noreply.github.com>
2020-09-13 23:10:41 +0200

    Refactoring of ContactUtils.kt

--- app/src/main/java/com/example/birthdayreminder/ContactUtils.kt
+++ app/src/main/java/com/example/birthdayreminder/ContactUtils.kt
@@ -6,38 +6,45 @@ import android.os.Build
 import android.provider.ContactsContract
 import androidx.annotation.RequiresApi
 
+
 @RequiresApi(Build.VERSION_CODES.O)
-public fun getContactIdByIndex(context: Context, index: Int): Int? {
+fun getContactIdByIndex(context: Context, index: Int): Int? {
     val contactsCursor = getContacts(context)
 
     if (contactsCursor !== null) {
-        contactsCursor.move(index + 1);
-
-        val idColumnIndex = contactsCursor.getColumnIndex("contact_id")
-
-        val text = contactsCursor.getInt(idColumnIndex)
-        return text
+        return getContactIdByIndex(contactsCursor, index);
     }
     return null
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
-public fun getContactNameByIndex(context: Context, index: Int): String? {
+fun getContactIdByIndex(contactsCursor: Cursor, index: Int): Int? {
+    contactsCursor.move(index + 1);
+    val idColumnIndex = contactsCursor.getColumnIndex("contact_id")
+    return contactsCursor.getInt(idColumnIndex)
+}
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun getContactNameByIndex(context: Context, index: Int): String? {
     val contactsCursor = getContacts(context)
 
     if (contactsCursor !== null) {
-        contactsCursor.move(index + 1);
-
-        val idColumnIndex = contactsCursor.getColumnIndex("display_name")
-
-        val text = contactsCursor.getString(idColumnIndex)
-        return text
+        return getContactNameByIndex(contactsCursor, index);
     }
     return null
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
-public fun getContacts(context: Context): Cursor? {
+fun getContactNameByIndex(contactsCursor: Cursor, index: Int): String? {
+    contactsCursor.move(index + 1);
+    val idColumnIndex = contactsCursor.getColumnIndex("display_name")
+    return contactsCursor.getString(idColumnIndex)
+}
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun getContacts(context: Context): Cursor? {
 
     // Sets the columns to retrieve for the user profile
     val projection = arrayOf(
