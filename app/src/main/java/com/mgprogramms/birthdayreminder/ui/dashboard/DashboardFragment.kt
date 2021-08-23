28db5368cd20c411fa7fb1e18a898f7e4c3d005c
Mario <marioegbertgeier@gmail.com>
2021-08-23 22:54:55 +0200

    Add contact birthday list

--- app/src/main/java/com/mgprogramms/birthdayreminder/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/mgprogramms/birthdayreminder/ui/dashboard/DashboardFragment.kt
@@ -1,15 +1,25 @@
 package com.mgprogramms.birthdayreminder.ui.dashboard
 
+import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CursorAdapter
+import android.widget.SimpleCursorAdapter
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.mgprogramms.birthdayreminder.R
+import com.mgprogramms.birthdayreminder.createFriendlyDate
 import com.mgprogramms.birthdayreminder.databinding.FragmentDashboardBinding
+import com.mgprogramms.birthdayreminder.getContactIdByIndex
+import com.mgprogramms.birthdayreminder.getContacts
 
 class DashboardFragment : Fragment() {
 
@@ -20,26 +30,103 @@ class DashboardFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        binding.contactList.setOnItemClickListener { adapterView, view, i, l ->
+            val contactId = getContactIdByIndex(requireContext(), i)
+            if (contactId != null) {
+                showContactDetail(contactId)
+            }
+        }
+        binding.contactList.setOnItemLongClickListener { adapterView, view, i, l ->
+            val contactId = getContactIdByIndex(requireContext(), i)
+            if (contactId != null) {
+                showEditContact(contactId)
+            }
+            true
+        }
+
+        loadContactList()
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun loadContactList() {
+
+        val contactsCursor = getContacts(requireContext());
+
+        val data = arrayOf(R.id.contact_name, R.id.birthday)
+
+        if (contactsCursor !== null) {
+
+            val adapter = SimpleCursorAdapter(
+                requireContext(),
+                R.layout.fragment_contact_list_item,
+                contactsCursor,
+                arrayOf("display_name", "data1"),
+                data.toIntArray(),
+                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER
+            )
+            binding.contactList.adapter = adapter
+            adapter.viewBinder = SimpleCursorAdapter.ViewBinder { a: View, b: Cursor, c: Int ->
+                setViewValue(
+                    a as TextView, b, c
+                )
+            }
+        }
+    }
+
+    private fun showEditContact(contactId: Int) {
+        val intent = Intent(Intent.ACTION_EDIT)
+        val uri: Uri = Uri.withAppendedPath(
+            ContactsContract.Contacts.CONTENT_URI,
+            contactId.toString()
+        )
+        intent.data = uri
+        this.startActivity(intent)
+    }
+
+    private fun showContactDetail(contactId: Int) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        val uri: Uri = Uri.withAppendedPath(
+            ContactsContract.Contacts.CONTENT_URI,
+            contactId.toString()
+        )
+        intent.data = uri
+        this.startActivity(intent)
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun setViewValue(
+        aView: TextView,
+        aCursor: Cursor,
+        aColumnIndex: Int
+    ): Boolean {
+        if (aColumnIndex == 2) {
+            val createDate: String = aCursor.getString(aColumnIndex)
+
+            val friendlyDate = createFriendlyDate(createDate)
+            aView.text = friendlyDate
+            return true
+        }
+        return false
     }
 }
