0480cd3c1aa50a0659c687a0cfad5c457478227e
Mario <marioegbertgeier@gmail.com>
2021-08-24 18:11:38 +0200

    Remove import

--- app/src/main/java/com/mgprogramms/birthdayreminder/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/mgprogramms/birthdayreminder/ui/notifications/NotificationsFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.mgprogramms.birthdayreminder.BirthdayNotificationWorker
 import com.mgprogramms.birthdayreminder.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
