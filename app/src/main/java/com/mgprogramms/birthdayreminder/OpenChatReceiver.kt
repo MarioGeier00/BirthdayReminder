1921d56b88eaf482e1e522ba56b7fda19616c995
MarioGeier00 <55717820+MarioGeier00@users.noreply.github.com>
2021-09-14 18:57:41 +0200

    Add default text when chat is opened

--- app/src/main/java/com/mgprogramms/birthdayreminder/OpenChatReceiver.kt
+++ app/src/main/java/com/mgprogramms/birthdayreminder/OpenChatReceiver.kt
@@ -5,20 +5,27 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat.startActivity
+import com.mgprogramms.birthdayreminder.birthday.Contacts.Companion.getContactNameByIndex
 import com.mgprogramms.birthdayreminder.birthday.Contacts.Companion.getPhoneNumberByContactId
+import java.net.URLEncoder
 
 const val CONTACT_ID = "contactId"
 
 class OpenChatReceiver : BroadcastReceiver() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onReceive(context: Context, intent: Intent) {
         val contactId = intent.extras?.get(CONTACT_ID) as Int?
         if (contactId != null) {
             val phoneNumber = getPhoneNumberByContactId(context, contactId)
+            val name = getContactNameByIndex(context, contactId)
 
-            val url = "https://api.whatsapp.com/send?phone=$phoneNumber"
+            val message = "Hallo $name! Ich wünsche dir alles Gute zu deinem Geburtstag!"
+            val url = "https://api.whatsapp.com/send?phone=$phoneNumber&text=${URLEncoder.encode(message, "UTF-8")}"
             try {
                 context.packageManager.getPackageInfo("com.whatsapp", PackageManager.GET_ACTIVITIES)
                 val newActivity = Intent(Intent.ACTION_VIEW).apply {
