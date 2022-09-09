265b15f9842d6d9775f9bebb1553f168ede6ed47
MarioGeier00 <marioegbertgeier@gmail.com>
2022-09-09 17:09:26 +0200

    remove unused code

--- app/src/main/java/de/mgprogramms/birthdayreminder/receivers/RemoveNotificationReceiver.kt
+++ app/src/main/java/de/mgprogramms/birthdayreminder/receivers/RemoveNotificationReceiver.kt
@@ -3,10 +3,7 @@ package de.mgprogramms.birthdayreminder.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
-import android.os.Build
 import androidx.core.app.NotificationManagerCompat
-import java.time.LocalDate
 
 
 class RemoveNotificationReceiver : BroadcastReceiver() {
@@ -17,52 +14,10 @@ class RemoveNotificationReceiver : BroadcastReceiver() {
                 with(NotificationManagerCompat.from(context)) {
                     cancel(notificationId)
                 }
-
-                with(getSharedPref(context).edit()) {
-                    putString(notificationId.toString(), LocalDate.now().toString())
-                    apply()
-                }
             }
     }
 
     companion object {
         const val NOTIFICATION_ID = "notificationId"
-        private const val IS_ACTIVATED = "isActivated"
-
-
-        fun getSharedPref(context: Context): SharedPreferences {
-            return context.getSharedPreferences(
-                "BirthdayReminderClosedNotifications",
-                Context.MODE_PRIVATE
-            )
-        }
-
-        fun isActivated(context: Context): Boolean {
-            return with(getSharedPref(context)) {
-                getBoolean(IS_ACTIVATED, false)
-            }
-        }
-
-        fun setActivatedState(context: Context, activated: Boolean) {
-            with(getSharedPref(context).edit()) {
-                if (!activated) {
-                    clear()
-                }
-                putBoolean(IS_ACTIVATED, activated)
-                apply()
-            }
-        }
-
-        fun hasBeenRemovedToday(context: Context, notificationId: Int): Boolean {
-            val sharedPref = getSharedPref(context)
-            val dismissDate = sharedPref.getString(notificationId.toString(), null)
-            val today = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                LocalDate.now().toString()
-            } else {
-                TODO("VERSION.SDK_INT < O")
-            }
-
-            return dismissDate == today
-        }
     }
 }
